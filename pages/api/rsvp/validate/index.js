809858821988e3c99613e77e7efd46f1d5832e0a
gordonwoodbine <gordon_woodbine@yahoo.co.uk>
2023-11-21 08:36:05 +0000

    add unique check to generated code, show snackbar if rsvp code not recognised

--- pages/api/rsvp/validate/index.js
+++ pages/api/rsvp/validate/index.js
@@ -9,32 +9,27 @@ const opts = {
 };
 
 export default async function handler(req, res) {
-  const { method, body: data } = req;
+  const {
+    method,
+    body: { rsvpCode },
+  } = req;
   await dbConnect();
 
   switch (method) {
     case 'POST':
       try {
-        const guest = await Guest.findOne({
-          rsvpCode: data,
-        });
+        const guest = await Guest.findOne({ rsvpCode });
+        let token;
+
         if (guest) {
-          jwt.sign({ user: data }, SECRET_KEY, opts, function (err, token) {
-            res.status(200).json({
-              success: true,
-              token,
-              guest,
-            });
-            return;
-          });
+          token = jwt.sign({ user: rsvpCode }, SECRET_KEY, opts);
+          res.status(200).json({ success: true, token, guest });
         } else {
-          res.status(404).json({
-            success: false,
-            message: 'Not found',
-          });
+          res.status(404).json({ success: false, message: 'Not found' });
         }
       } catch (err) {
-        console.log(err);
+        console.log('err', err);
+        res.status(500).json({ success: false, message: 'Server error' });
       }
       break;
     default:
